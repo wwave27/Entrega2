c17f0df2dbecdecd4c0f6e727065c14711cfd8da
joaarayav <joa.arayav@gmail.com>
2020-11-01 00:04:06 -0300

    enlace entre paginas

--- trabajoBD/urls.py
+++ trabajoBD/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path,include
 from trabajoBD import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('index/', views.home, name="index"),
