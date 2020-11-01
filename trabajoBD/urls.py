173a95435b09da7c027485d121ec4487932532fc
joaarayav <joa.arayav@gmail.com>
2020-10-31 22:50:03 -0300

    paginas

--- trabajoBD/urls.py
+++ trabajoBD/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('index/', views.home, name="index"),
     path('registrarse/', views.register, name="registrarse"),
     path('ps5/', views.ps5news, name="ps5"),
-    path('switch/', views.switchnews, name="switch")
-    path('pc/', views.switchnews, name="pc")
-    path('retro/', views.switchnews, name="retro")
-    path('xbox/', views.switchnews, name="xbox")
+    path('switch/', views.switchnews, name="switch"),
+    path('pc/', views.switchnews, name="pc"),
+    path('retro/', views.switchnews, name="retro"),
+    path('xbox/', views.switchnews, name="xbox"),
 ]
